cb6e462432bf330fecb9c03116f30c69c987a16b
Manuel Trinidad <manuel.trinidad.mo@gmail.com>
2022-07-01 18:51:44 +0300

    🌋 The app is reborn!!1!

--- frontend/src/Type/User.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-// eslint-disable-next-line import/prefer-default-export
-export const userType = PropTypes.shape({
-  name: PropTypes.string,
-  email: PropTypes.string,
-  handle: PropTypes.string,
-  isAdmin: PropTypes.bool
-});
