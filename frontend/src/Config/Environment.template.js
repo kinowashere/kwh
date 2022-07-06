23d68397962febe12d775c348756aa8315a73c4a
Manuel Trinidad <manuel.trinidad.mo@gmail.com>
2022-07-06 18:47:52 +0300

    🦖 Rawr! Switch to deno with fresh

--- frontend/src/Config/Environment.template.js
+++
@@ -1,8 +0,0 @@
-// URL & Routing
-export const API_URL = '';
-export const FRONTEND_URL = '';
-export const API_ROUTE = 'api';
-export const API_VERSION = '1';
-
-// General App Info
-export const APP_NAME = 'KiNO Was Here';
--- frontend/src/index.js
+++
@@ -1,5 +0,0 @@
-import { render } from 'react-dom';
-import 'Style/Main.scss';
-import App from 'Component/App';
-
-render(<App />, document.getElementById('root'));
